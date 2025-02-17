b5e4a1e287d62120c6355878808726ddc82f4b87
Monstazim <nikitadozuy@gmail.com>
2025-02-17 14:00:29 +0300

    Create_lab_1

---
+++ Lab1/Lab1/Lab1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <stdio.h>;
+struct Stack
+{ 
+	int Data;
+	Stack* next;
+};
+void s_push(Stack** top, int D) 
+{
+	Stack* q; 
+	q = new Stack(); 
+	q->Data = D; 
+	if (top == NULL) 
+	{ 
+		*top = q;
+	}
+	else 
+	{
+		q->next = *top; 
+		*top = q; 
+	}
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+}
